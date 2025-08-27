c7deea96e7c5cfa32516f46f3c88e7bbe4fe118d
Ayobami Tunwase <tunwaseayobami98@gmail.com>
2025-08-27 09:20:50 +0100

    Implemented the dashboard layout.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,11 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
 import { AppSidebar } from "@/app/dashboard/(sidebar)/app-sidebar";
 
+import { Bell, CircleQuestionMark, Moon } from "lucide-react";
 
 export default function DashboardLayout({
   children,
@@ -12,10 +15,26 @@ export default function DashboardLayout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <header>
-        <SidebarTrigger />
-      </header>
-      <main className="w-full">{children}</main>
+
+      <div className="flex flex-col w-full">
+        <header className="flex justify-between border-b border-border py-2 px-3 items-center gap-4">
+          <SidebarTrigger />
+
+          <div className="flex items-center gap-6">
+            <CircleQuestionMark className="w-5 h-5" />
+            <Bell className="w-5 h-5" />
+            <Moon className="w-5 h-5" />
+
+            <Avatar className="h-10 w-10 rounded-full">
+              <AvatarImage src="htt://github.com/shadcn.png" />
+              <AvatarFallback className="bg-secondary text-secondary-foreground leading-6">
+                OD
+              </AvatarFallback>
+            </Avatar>
+          </div>
+        </header>
+        <main className="w-full">{children}</main>
+      </div>
 
       <Toaster />
     </SidebarProvider>
