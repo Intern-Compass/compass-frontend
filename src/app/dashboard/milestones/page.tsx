8243df95a7c1de8387627719fa44b82e43ba1010
Ayobami Tunwase <tunwaseayobami98@gmail.com>
2025-08-28 15:25:12 +0100

    Completed the milestones page.

--- src/app/dashboard/milestones/page.tsx
+++ src/app/dashboard/milestones/page.tsx
@@ -5,42 +5,46 @@ export default function MilestonesPage() {
   return (
     <div className="bg-card p-8">
       <header>
-        <div className="flex items-center gap-1.5">
-          <Clock1 className="w-6 h-6" />
-          <h1 className="leading-8 text-2xl font-medium">Milestone</h1>
-
-          <div>
-            <Calendar />
-            <span>Date</span>
-            <span className="text-secondary-foreground bg-muted py-0.5 px-1 rounded-sm">26th May - 29th May 2025</span>
+        <div className="bg-card rounded py-4">
+          <div className="flex items-center gap-1.5 mb-5 ml-4">
+            <Clock1 className="w-6 h-6" />
+            <h1 className="leading-8 text-2xl font-medium">Milestone</h1>
           </div>
-        </div>
-      </header>
-      <main>
-        <section className="rounded-2xl">
-          <div className="flex gap-6 max-w-[1057px]">
-            <div className="grow bg-white p-6 rounded-xl space-y-6">
-              <div className="flex items-center gap-5">
-                <CircleCheckBig />
+
+          <div className="mb-8 flex items-center gap-2 text-xs rounded-md border border-dashed border-border py-2 px-3 w-fit">
+            <Calendar className="w-4 h-4" />
+            <span className="leading-5">Date:</span>
+            <span className="text-secondary-foreground bg-muted py-0.5 px-1 rounded-sm leading-4">
+              26th May - 29th May 2025
+            </span>
+          </div>
+          <div className="grid gap-6 max-w-[1057px] md:grid-cols-[repeat(auto-fit,_minmax(330px,1fr))]">
+            <div className="bg-white p-6 rounded-xl space-y-6">
+              <div className="flex items-center gap-6">
+                <CircleCheckBig className="w-10 h-10 text-[rgb(173,138,0)] bg-[rgb(255,247,217)] p-2.5 rounded-full" />
                 <span className="font-medium leading-6">Completed</span>
               </div>
               <span className="font-medium text-4xl leading-10">0</span>
             </div>
-            <div className="grow bg-white p-6 rounded-xl space-y-6">
-              <div className="flex items-center gap-5">
-                <CircleDot />
+            <div className="bg-white p-6 rounded-xl space-y-6">
+              <div className="flex items-center gap-6">
+                <CircleDot className="w-10 h-10 text-[rgb(173,138,0)] bg-[rgb(255,247,217)] p-2.5 rounded-full" />
                 <span className="font-medium leading-6">Upcoming</span>
               </div>
               <span className="font-medium text-4xl leading-10">0</span>
             </div>
-            <div className="grow bg-white p-6 rounded-xl space-y-6">
-              <div className="flex items-center gap-5">
-                <Ban />
+            <div className="bg-white p-6 rounded-xl space-y-6">
+              <div className="flex items-center gap-6">
+                <Ban className="w-10 h-10 text-[rgb(173,138,0)] bg-[rgb(255,247,217)] p-2.5 rounded-full" />
                 <span className="font-medium leading-6">Overdue</span>
               </div>
               <span className="font-medium text-4xl leading-10">0</span>
             </div>
           </div>
+        </div>
+      </header>
+      <main>
+        <section className="rounded-2xl">
           <figure>
             <div className="relative max-w-[930px] min-h-[300px]">
               <Image
